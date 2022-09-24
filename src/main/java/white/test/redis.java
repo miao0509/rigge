ceec656c0617022a7b747e350757e9687ebc3029
white <2372055182@qq.com>
2022-09-24 17:17:40 +0800

    add redis

---
+++ src/main/java/white/test/redis.java
@@ -0,0 +1,19 @@
+package white.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+public class redis {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test() {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("name", "wenwen");
+    }
+}
